17724a39f0429b8da3d873128314e2b241df9358
Chia <chia@punters.com.au>
2019-12-22 23:06:41 +1100

    chore: add passport local strategy, login and logout handler.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
+const { dbPassword } = require('./password');
+
 module.exports = {
-  MongoURI:
-    "mongodb+srv://chia-tsai-001:1U9^bXYPoCjItPr@cluster0-m2onl.mongodb.net/test?retryWrites=true&w=majority"
+  MongoURI: `mongodb+srv://chia-tsai-001:${dbPassword}@cluster0-m2onl.mongodb.net/test?retryWrites=true&w=majority`
 };
--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-
-// User model
+const passport = require('passport');
+// Load User model
 const User = require('../models/User');
+const { forwardAuthenticated } = require('../config/auth');
 
-// Login page
-router.get('/login', (req, res) => res.render('login'));
+// Login Page
+router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
 
 // Register Page
-router.get('/register', (req, res) => res.render('register'));
+router.get('/register', forwardAuthenticated, (req, res) =>
+  res.render('register')
+);
 
-// Register Handle
+// Register
 router.post('/register', (req, res) => {
   const { name, email, password, password2 } = req.body;
   let errors = [];
 
-  // Check required fields
   if (!name || !email || !password || !password2) {
-    errors.push({ msg: 'Please fill in all fields' });
+    errors.push({ msg: 'Please enter all fields' });
   }
 
-  // Check passwords match
-  if (password !== password2) {
+  if (password != password2) {
     errors.push({ msg: 'Passwords do not match' });
   }
 
-  // Check pass length
   if (password.length < 6) {
-    errors.push({ msg: 'Passowrd should be at least 6 characters' });
+    errors.push({ msg: 'Password must be at least 6 characters' });
   }
 
   if (errors.length > 0) {
@@ -40,11 +40,9 @@ router.post('/register', (req, res) => {
       password2
     });
   } else {
-    // Validation passed
     User.findOne({ email: email }).then(user => {
       if (user) {
-        // User exists
-        errors.push({ msg: 'Email is already registered' });
+        errors.push({ msg: 'Email already exists' });
         res.render('register', {
           errors,
           name,
@@ -58,7 +56,7 @@ router.post('/register', (req, res) => {
           email,
           password
         });
-        // Hash Password
+
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
             if (err) throw err;
@@ -78,6 +76,22 @@ router.post('/register', (req, res) => {
       }
     });
   }
+});
+
+// Login
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
 });
 
 module.exports = router;
